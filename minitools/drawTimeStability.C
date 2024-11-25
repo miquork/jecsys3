00934ea411156a988b5a34d374e770c2c3554ab1
Mikko Voutilainen <voutila@voutila-24.dyndns.cern.ch>
2024-11-25 21:53:09 +0100

    Update drawTimeStability*.C used in PPD workshop.

--- minitools/drawTimeStability.C
+++ minitools/drawTimeStability.C
@@ -32,34 +32,70 @@ void drawTimeStability() {
 
   // Listing all files here. GamJet and ZmmJet switch filetype
   string vf[] = {
+
+    // dijet
+    "DiJet",
+    // //"2022C_v39","2022D_v39","2022E_v39","2022F_v39","2022G_v39",
+    //"2022C_v113","2022D_v113","2022E_v113","2022F_v113","2022G_v113",
+    //"2023Cv123_v39","2023Cv4_v39","2023D_v39",
+    // //"2024B_v110","2024C_v110","2024D_v110","2024E_v110",
+    // //"2024F_v110","2024G_v110","2024H_v111","2024I_v111",
+    //"2024B_v112","2024C_v112","2024D_v112","2024E_v112",
+    //"2024F_v112","2024G_v112","2024H_v112","2024I_v112",
+    "2022_v113","2023_v113","2024_v113",
+
+    
     // gamma+jet
     "GamJet",
-    "2022CDE_v32","2022FG_v32",
-    "2023Cv123_w8","2023Cv4_w8","2023D_w8",
-    "2024BCD_w39","2024E_w39","2024F_w39","2024G_w39","2024H_w40","2024I_w40",
+    "2022C","2022D","2022E","2022F","2022G",
+    "2023Cv123","2023Cv4","2023D",
+    "2024B","2024C","2024D","2024Ev1","2024Ev2","2024F","2024G","2024H","2024Iv1","2024Iv2",
+    //"2022CDE_v32","2022FG_v32",
+    //"2023Cv123_w8","2023Cv4_w8","2023D_w8",
+    //"2024BCD_w39","2024E_w39","2024F_w39","2024G_w39","2024H_w40","2024I_w40",
+
+    
     // Z+jet
-    "ZmmJet",
-    "2022CD","2022E","2022FG",
+    "ZmmJet",    
+    "2022CD","2022E",//"2022FG",
+    "2022F","2022G",
     "2023C123","2023C4","2023D",
     "2024BCD","2024E","2024F","2024G","2024H","2024I"
+
   };
   const int nf = sizeof(vf)/sizeof(vf[0]);
 
   // Listing all histograms here. GamJet and ZmmJet switch filetype
   string vh[] = {
+    /*
+    // Dijet
+    "DiJet",
+    "h1jetrate"
+    */
+
     // Gamma+jet
     "GamJet",
+    //"pr50m_eta08hi","pr50m_eta08lo",
+    //"pr50m_eta3to4","pr50m_eta4to5",
+    //"pr110m_eta3to4","pr110m_eta4to5",
     "pr50n","pr110n","pr230n",
+    /*
     "pr230m","pr110m","pr50m",
     "pr50nhf","pr50nef","pr50chf",
     "pr110nhf","pr110nef","pr110chf",
     "pr230nhf","pr230nef","pr230chf",
+    */
+
     // Zmm+jet
     "ZmmJet",
-    "mpf_run_zpt110","mpf_run_zpt50","mpf_run_zpt30",
+    //"mz_run_zpt0","mz_run_zpt30","mz_run_zpt50","mz_run_zpt110",
+    "nz_run_zpt0","nz_run_zpt30","nz_run_zpt50","nz_run_zpt110",
+    /*
+    "mpf_run_zpt30","mpf_run_zpt110","mpf_run_zpt50",
     "chf_run_zpt30","nef_run_zpt30","nhf_run_zpt30",
     "chf_run_zpt50","nef_run_zpt50","nhf_run_zpt50",
     "chf_run_zpt110","nef_run_zpt110","nhf_run_zpt110",
+    */
   };
   const int nh = sizeof(vh)/sizeof(vh[0]);
 
@@ -108,16 +144,19 @@ void drawTimeStability() {
   TH1D *hscales = new TH1D("hscales",";Observable;Scale",2*nh,0,2*nh);
   
   // Loop over files to retrieve stuff
-  bool useGam(false), useZmm(false);
+  bool useGam(false), useZmm(false), useJet(false);
   for (int ih = 0; ih != nh; ++ih) {
 
     string sh = vh[ih];
     const char *ch = sh.c_str();
     TString th(ch);
     cout << "Analyzing " << ch << endl << flush;
-    if (sh=="GamJet") { useGam = true; useZmm = false; continue; }
-    if (sh=="ZmmJet") { useGam = false; useZmm = true; continue; }
+    if (sh=="GamJet") { useGam = true; useZmm = false; useJet=false; continue; }
+    if (sh=="ZmmJet") { useGam = false; useZmm = true; useJet=false; continue; }
+    if (sh=="DiJet")  { useGam = false; useZmm = false; useJet=true; continue; }
     bool isPF = (th.Contains("nhf") || th.Contains("nef") || th.Contains("chf"));
+    bool isZmass = (th.Contains("mz_run"));
+    bool isEta = (th.Contains("eta"));
     double kpf(1);
     if (th.Contains("nhf")) kpf = 20;  // 5%
     if (th.Contains("nef")) kpf = 4;   // 25%
@@ -135,31 +174,62 @@ void drawTimeStability() {
     // Keep track of JES (1=L2L3Res) for gamma+jet MPF
     //TH1D *hjes = new TH1D(Form("hjes_%s",ch),";JES;Cum.Lum(/fb)",hbins->GetNbinsX(),&vx[0]);
 
-    bool isGam(false), isZmm(false);
+    bool isGam(false), isZmm(false), isJet(false);
     for (int iff = 0; iff != nf; ++iff) {
 
       string sf = vf[iff];
       const char *cf = sf.c_str();
       TString tf(cf);
       
-      if (sf=="GamJet") { isGam = true; isZmm = false; continue; }
-      if (sf=="ZmmJet") { isGam = false; isZmm = true; continue; }
+      if (sf=="GamJet") { isGam=true; isZmm=false; isJet=false; continue; }
+      if (sf=="ZmmJet") { isGam=false; isZmm=true; isJet=false; continue; }
+      if (sf=="DiJet")  { isGam=false; isZmm=false; isJet=true; continue; }
       if (useGam && !isGam) continue;
       if (useZmm && !isZmm) continue;
+      if (useJet && !isJet) continue;
       
       TFile *f(0);
       // Photon+jet files
       if (useGam) {
+	/*
 	if (tf.Contains("2024")) f = new TFile(Form("rootfiles/Prompt2024/GamHistosFill_data_%s.root",cf),"READ");
 	if (tf.Contains("2023")) f = new TFile(Form("rootfiles/Summer23_L2L3Res/GamHistosFill_data_%s.root",cf),"READ");
 	if (tf.Contains("2022")) f = new TFile(Form("../gamjet/rootfiles/GamHistosFill_data_%s.root",cf),"READ");
+	*/
+	f = new TFile(Form("rootfiles/Prompt2024/GamHistosFill_data_%s_w42.root",cf),"READ");
       }
       // Zmm+jet files
       if (useZmm) {
+	/*
 	if (tf.Contains("2022FG")) f = new TFile(Form("rootfiles/jme_bplusZ_%s_Zmm_sync_v78.root",cf),"READ");
 	else if (tf.Contains("2022"))   f = new TFile(Form("rootfiles/jme_bplusZ_%s_Zmm_sync_v76.root",cf),"READ");
 	if (tf.Contains("2023"))   f = new TFile(Form("rootfiles/Prompt2024/jme_bplusZ_%s_Zmm_sync_v84.root",cf),"READ");
 	if (tf.Contains("2024"))   f = new TFile(Form("rootfiles/Prompt2024/v88/jme_bplusZ_%s_Zmm_sync_v88.root",cf),"READ");
+	*/
+	
+	//if (tf.Contains("2024F")) f = new TFile(Form("rootfiles/Prompt2024/v88/jme_bplusZ_%s_Zmm_sync_v88.root",cf),"READ");
+	if (tf.Contains("2024BC") || tf.Contains("2024F") || tf.Contains("2024G") || tf.Contains("2024H")) f = new TFile(Form("rootfiles/Prompt2024/v91/jme_bplusZ_%s_Zmm_sync_v91d.root",cf),"READ");
+	else
+	  f = new TFile(Form("rootfiles/Prompt2024/v91/jme_bplusZ_%s_Zmm_sync_v91.root",cf),"READ");
+      }
+      if (useJet) {
+	char cv[256], ce[256];
+	TString tf(cf); tf.ReplaceAll("_"," ");
+	sscanf(tf.Data(),"%s %s",ce,cv);
+	cout << "s="<<cf<<", ce="<<ce<<", cv="<<cv<<endl<<flush;
+	/*
+	if (tf.Contains("2024")) {
+	  f = new TFile(Form("rootfiles/Prompt2024/%s_2024/jmenano_data_out_%s_JME_%s_2024.root",cv,ce,cv),"READ");
+	}
+	if (tf.Contains("2023")) {
+	  f = new TFile(Form("rootfiles/Summer23_L2L3ResJERSF/%s_2023_etabin_SFv2/jmenano_data_out_%s_JME_%s_2023_etabin_SFv2.root",cv,ce,cv),"READ");
+	}
+	if (tf.Contains("2022")) {
+	  //f = new TFile(Form("rootfiles/Summer22_L2L3ResJERSF/%s_2022_etabin_SFv/jmenano_data_out_%s_JME_%s_2022_etabin_SFv.root",cv,ce,cv),"READ");
+	  f = new TFile(Form("rootfiles/Prompt2024/%s_2022/jmenano_data_out_%s_nib1_JME_%s_2022.root",cv,ce,cv),"READ");
+	}
+	*/
+	f = new TFile(Form("rootfiles/Prompt2024/%s_%s/jmenano_data_out_%s_%s.root",cv,ce,ce,cv));
       }
       if (!f || f->IsZombie()) cout << "Missing " << cf << endl << flush;
       assert(f && !f->IsZombie());
@@ -168,8 +238,9 @@ void drawTimeStability() {
       TH1D *h(0);
       TProfile *p(0);
       TObject *o(0);
-      if (useGam) o = f->Get(Form("runs/%s",ch));
+      if (useGam) o = f->Get(Form("runs%s/%s",isEta ? "_high-jeteta" : "",ch));
       if (useZmm) o = f->Get(Form("data/%s",ch));
+      if (useJet) o = f->Get(Form("HLT_PFJet500/JetsperRuns/%s",ch));
       // Detect object type automatically
       if (o) {
 	if (o->InheritsFrom("TProfile"))  p = (TProfile*)o;
@@ -180,12 +251,22 @@ void drawTimeStability() {
       TProfile2D *p2res(0);
       if (useGam) p2res = (TProfile2D*)f->Get("Gamjet2/p2res");
       if (useZmm) p2res = (TProfile2D*)f->Get("data/l2res/p2res");
+      if (useJet) p2res = (TProfile2D*)f->Get("HLT_PFJet500/Dijet2/p2res");
       if (!p2res) cout << "Missing p2res in " << cf << endl << flush;
       double jes(1.);
       if (p2res) {
 	// Slice out [110,230]x[-1.3,1.3] region to get mean JES here
-	int ipt1 = p2res->GetYaxis()->FindBin(110.);
-	int ipt2 = p2res->GetYaxis()->FindBin(230.);
+	//int ipt1 = p2res->GetYaxis()->FindBin(110.);
+	//int ipt2 = p2res->GetYaxis()->FindBin(230.);
+	double pt1(110.), pt2(230.);
+	TString th(ch);
+	if (th.Contains("230"))      { pt1 = 230; pt2=500; }
+	else if (th.Contains("110")) { pt1 = 110; pt2=230; }
+	else if (th.Contains("50"))  { pt1 = 50;  pt2=110; }
+	else if (th.Contains("30"))  { pt1 = 30;  pt2=50;  }
+	int ipt1 = p2res->GetYaxis()->FindBin(pt1);
+	int ipt2 = p2res->GetYaxis()->FindBin(pt2);
+	
 	TProfile *px = p2res->ProfileX("px",ipt1,ipt2);
 	int ieta1 = p2res->GetXaxis()->FindBin(-1.3);
 	int ieta2 = p2res->GetXaxis()->FindBin(+1.3);
@@ -198,8 +279,9 @@ void drawTimeStability() {
       if (h) {
 	for (int i = 1; i != h->GetNbinsX()+1; ++i) {
 	  int j = hsum->FindBin(h->GetBinCenter(i));
-	  hsum->SetBinContent(j, hsum->GetBinContent(j) + h->GetBinContent(i));
-	  hsum->SetBinError(j, sqrt(pow(hsum->GetBinError(j),2) + pow(h->GetBinError(i),2)));
+	  double k = 1;//(useJet && tf.Contains("2022") ? 12 : 1);
+	  hsum->SetBinContent(j, hsum->GetBinContent(j) + k*h->GetBinContent(i));
+	  hsum->SetBinError(j, sqrt(pow(hsum->GetBinError(j),2) + pow(k*h->GetBinError(i),2)));
 	} // for i
       } // if h
 	       
@@ -252,9 +334,21 @@ void drawTimeStability() {
 
     double xmin = hbins->GetXaxis()->GetXmin();
     double xmax = hbins->GetXaxis()->GetXmax();
-    double kf = (psum->Integral()!=0 ? (isPF ? kpf : 1.) : 20.);
+    //double kf = (psum->Integral()!=0 ? (isPF ? kpf : 1.) : 20.);
+    double kf = (psum->Integral()!=0 ? (isPF ? kpf : 1.) : 12.);
     double ymin = (psum->Integral()!=0 ? (isPF ? -2.5*kpf : -2.5) : -2.5*kf);
     double ymax = (psum->Integral()!=0 ? (isPF ? +5.5*kpf : +5.5) : +5.5*kf);
+    if (isZmass) {
+      ymin = -0.2; ymax = +0.3;
+    }
+    //if (useJet) {
+    //ymin = -200; ymax=+400;
+    //}
+    if (isEta) {
+      //ymin = -5; ymax = +11;
+      //ymin = -7.5; ymax = +16.5;
+      ymin = -10; ymax = +22;
+    }
     //TH1D *h1 = tdrHist(Form("h1_%s",ch),"JES",0.975,1.025,"Cumulative luminosity (fb^{-1})",0,hbins->GetXaxis()->GetXmax());
     TH1D *h1 = tdrHist(Form("h1_%s",ch),"JES-1 (%)",ymin,ymax,"Cumulative luminosity (fb^{-1})",xmin,xmax);
     lumi_136TeV = "Run3, 2022-24";
@@ -271,6 +365,11 @@ void drawTimeStability() {
       l->SetLineStyle(kDotted);
       l->DrawLine(xmin, +1*kf, xmax, +1*kf);
       l->DrawLine(xmin, -1*kf, xmax, -1*kf);
+
+      if (isZmass) {
+	l->DrawLine(xmin, +0.03*kf, xmax, +0.03*kf);
+	l->DrawLine(xmin, -0.03*kf, xmax, -0.03*kf);
+      }
 
       // Lines at era breaks
       // Define era boundaries as pairs of (start_run, end_run)
@@ -359,7 +458,7 @@ void drawTimeStability() {
       TLatex *tex = new TLatex();
       tex->SetTextSize(0.045);
       int ks(0), kh(0);
-      for (int i = 0; i != eras.size(); ++i) {
+      for (unsigned int i = 0; i != eras.size(); ++i) {
 
 	int run = eras[i].first;
 	string s = eras[i].second;
@@ -387,10 +486,18 @@ void drawTimeStability() {
 	if (s=="22"||s=="23"||s=="24") ks = 0;
 	//tex->DrawLatex(cumlum+1,-1.2-0.2*(ks++),cn);
 	//if (t.Contains("V")) tex->DrawLatex(cumlum1+1,-1.6-0.4*(kh++),cn);
-	if (t.Contains("V") || t.Contains("IC") || t.Contains("XX") || t.Contains("EP"))
-	  tex->DrawLatex(cumlum+1,(-1.6-0.4*(kh++%3))*kf,cn);
-	else
-	  tex->DrawLatex(cumlum+1,(+3.5-0.4*(ks++))*kf,cn);
+	if (t.Contains("V") || t.Contains("IC") || t.Contains("XX") || t.Contains("EP")) {
+	  if (isZmass) tex->DrawLatex(cumlum+1,(-0.12-0.03*(kh++%3))*kf,cn);
+	  else if (isEta) tex->DrawLatex(cumlum+1,(-3.2-0.8*(kh++%3))*kf,cn);
+	  //else if (isEta) tex->DrawLatex(cumlum+1,(-5.8-1.6*(kh++%3))*kf,cn);
+	  else tex->DrawLatex(cumlum+1,(-1.6-0.4*(kh++%3))*kf,cn);
+	}
+	else {
+	  if (isZmass) tex->DrawLatex(cumlum+1,(+0.15-0.03*(ks++))*kf,cn);
+	  else if (isEta) tex->DrawLatex(cumlum+1,(+7.0-0.8*(ks++))*kf,cn);
+	  //else if (isEta) tex->DrawLatex(cumlum+1,(+14.0-1.6*(ks++))*kf,cn);
+	  else tex->DrawLatex(cumlum+1,(+3.5-0.4*(ks++))*kf,cn);
+	}
 
 	hbreaks->SetBinContent(i+1,cumlum);
 	hbreaks->GetXaxis()->SetBinLabel(i+1,cn);
@@ -433,6 +540,7 @@ void drawTimeStability() {
     if (psum->Integral()!=0) {
       //f1->SetRange(82,1e6); // V2.0 onwards => wrong /fb
       f1->SetRange(90.7,1e6); // V2.0 onwards
+      f2->SetRange(90.7,1e6); // V2.0 onwards
       
       // Normalize average to unity to focus on time dependence
       psum->Fit(f1,"QRN");
@@ -453,18 +561,25 @@ void drawTimeStability() {
 	  hjes->SetBinError(i, hjes->GetBinError(i)*100.);
 	}
       }
-      
-      tdrDraw(hjes,"PE",kOpenSquare,kRed,kSolid,-1,kNone,0,0.6);
+
+      if (!isZmass)
+	tdrDraw(hjes,"PE",kOpenSquare,kRed,kSolid,-1,kNone,0,0.6);
       tdrDraw(h,"PE",kFullCircle,kGreen+2,kSolid,-1,kNone,0,0.6);
       
       TLegend *leg = tdrLeg(0.65,0.85-0.05*3,0.90,0.85);
       //leg->SetHeader("#gamma+jet MPF 110EB");
       leg->SetHeader(mhead[ch]);
-      leg->AddEntry(hjes,"Before JEC");
-      leg->AddEntry(h,"After JEC");
-      
+      if (isZmass) {
+	leg->AddEntry(h,"m_{Z} / #LTm_{Z}#GT");
+      }
+      else {
+	leg->AddEntry(hjes,"Before JEC");
+	leg->AddEntry(h,"After JEC");
+      }
+	
       fout->cd();
-      hjes->Write(Form("%s_jes",ch),TObject::kOverwrite);
+      if (!isZmass)
+	hjes->Write(Form("%s_jes",ch),TObject::kOverwrite);
       h->Write(Form("%s",ch),TObject::kOverwrite);
       curdir->cd();
     } // psum
--- minitools/drawTimeStabilityPairs.C
+++ minitools/drawTimeStabilityPairs.C
@@ -17,19 +17,23 @@ void cleanEmpty(TH1D *hd, TH1D *ha, TH1D *hb);
 double getScale(TH1D *hscales, string name);
 // Draw Gamma scale vs Zmm scale
 void drawGamVsZmm();
+void drawGamVsGam();
 void drawPFcomp(string ref);
 
 // Main call for drawing various pairs
 void drawTimeStabilityPairs() {
 
-  drawGamVsZmm();
-  
+  //drawGamVsZmm();
+
   drawPFcomp("pr50");
   drawPFcomp("pr110");
   drawPFcomp("pr230");
+  /*
   drawPFcomp("zpt30");
   drawPFcomp("zpt50");
   drawPFcomp("zpt110");
+  */
+  //drawGamVsGam();
 }
 
 void drawGamVsZmm() {
@@ -182,8 +186,156 @@ void drawGamVsZmm() {
   gPad->RedrawAxis();
   
   c1->SaveAs("pdf/drawTimeStability/drawTimeStabilityPairs_GamVsZmm.pdf");
-} // drawTimeStabilityPairs()
+} // drawGamVsZmm
 
+void drawGamVsGam() {
+  
+  setTDRStyle();
+  TDirectory *curdir = gDirectory;
+  
+  TFile *f = new TFile("rootfiles/drawTimeStability.root","READ");
+  assert(f && !f->IsZombie());
+
+  curdir->cd();
+
+  TLine *l = new TLine();
+  l->SetLineStyle(kDashed);
+  l->SetLineColor(kGray+1);
+
+  double xmin(0), xmax(175), ymin(-2.4), ymax(+3.4), ymind(-1.5), ymaxd(+1.9);
+  TH1D *h = tdrHist("h_u","JES-1 (%)",ymin,ymax,"Cumulative luminosity (fb^{-1})",xmin,xmax);
+  TH1D *h_d = tdrHist("h_d","#gamma+j - Z+j (%)",ymind,ymaxd,"Cumulative luminosity (fb^{-1})",xmin,xmax);
+  lumi_136TeV = "Run3, 2022-24";
+  extraText = "Private";
+  TCanvas *c1 = tdrDiCanvas("c1",h,h_d,8,11);
+
+  TH1D *hbreaks = (TH1D*)f->Get("hbreaks"); assert(hbreaks);
+  TH1D *hscales = (TH1D*)f->Get("hscales"); assert(hscales);
+
+  double jeshi(1), jeslo(1);
+  for (int i = 1; i != hscales->GetNbinsX()+1; ++i) {
+    string s = hscales->GetXaxis()->GetBinLabel(i);
+    if (s=="pr50m_eta08hi") jeshi = hscales->GetBinContent(i);
+    if (s=="pr50m_eta08lo") jeslo = hscales->GetBinContent(i);
+  }
+  
+  TH1D *ha = (TH1D*)f->Get("pr50m_eta08lo"); assert(ha);
+  ha->Scale(jeslo);
+  TH1D *hb = (TH1D*)f->Get("pr50m_eta08hi"); assert(hb);
+  hb->Scale(jeshi);
+  TH1D *hd = (TH1D*)ha->Clone("hd");
+  hd->Add(hb,-1);
+  cleanEmpty(hd,ha,hb);
+
+  c1->cd(1);
+
+  TLine *ll = new TLine();
+  TLatex *tex = new TLatex();
+  tex->SetTextSize(0.03);
+  set<double> breakset;
+  breakset.insert(xmin);
+  for (int i = 1; i != hbreaks->GetNbinsX()+1; ++i) {
+    double cumlum = hbreaks->GetBinContent(i);
+    TString ts(hbreaks->GetXaxis()->GetBinLabel(i));
+
+    ll->SetLineColor(kGray);
+    if (ts.Contains("V")) ll->SetLineColor(kRed-9);
+    if (ts.Contains("IC")) ll->SetLineColor(kBlue-9);
+    ll->DrawLine(cumlum,ymin,cumlum,ymax);
+
+    tex->SetTextColor(kGray);
+    if (ts.Contains("V")) tex->SetTextColor(kRed-9);
+    if (ts.Contains("IC")) tex->SetTextColor(kBlue-9);
+    tex->DrawLatex(cumlum+1,ymin+0.4-((i-1)%3)*0.15,ts.Data());
+
+    breakset.insert(cumlum);
+  }
+  breakset.insert(xmax);
+  
+  l->SetLineStyle(kDashed);
+  l->DrawLine(xmin,0,xmax,0);
+  l->SetLineStyle(kDotted);
+  l->DrawLine(xmin,+1,xmax,+1);
+  l->DrawLine(xmin,-1,xmax,-1);
+  
+  tdrDraw(hb,"Pz",kFullSquare,kRed,kSolid,-1,kNone,0,0.6);
+  tdrDraw(ha,"Pz",kFullCircle,kBlue,kSolid,-1,kNone,0,0.6);
+
+  TLegend *leg = tdrLeg(0.60,0.89-0.05*2,0.85,0.89);
+  leg->SetFillStyle(1001);
+  leg->AddEntry(hb,"#gamma+jet 50 |#eta_{#gamma}|>0.8","PLE");
+  leg->AddEntry(ha,"#gamma+jet 50 |#eta_{#gamma}|<0.8","PLE");
+
+  gPad->RedrawAxis();
+  gPad->Update();
+  
+  c1->cd(2);
+
+  tex->SetTextSize(0.03*1.5);
+  for (int i = 1; i != hbreaks->GetNbinsX()+1; ++i) {
+    double cumlum = hbreaks->GetBinContent(i);
+    TString ts(hbreaks->GetXaxis()->GetBinLabel(i));
+
+    ll->SetLineColor(kGray);
+    if (ts.Contains("V")) ll->SetLineColor(kRed-9);
+    if (ts.Contains("IC")) ll->SetLineColor(kBlue-9);
+    ll->DrawLine(cumlum,ymind,cumlum,ymaxd);
+
+    tex->SetTextColor(kGray);
+    if (ts.Contains("V")) tex->SetTextColor(kRed-9);
+    if (ts.Contains("IC")) {
+      tex->SetTextColor(kBlue-9);
+      tex->DrawLatex(cumlum+1,ymind+0.5,ts.Data());
+    }
+    //tex->DrawLatex(cumlum+1,ymind+0.4-((i-1)%3)*0.15,ts.Data());
+  }
+
+  l->SetLineStyle(kDashed);
+  l->DrawLine(xmin,0,xmax,0);
+  l->SetLineStyle(kDotted);
+  l->DrawLine(xmin,+0.5,xmax,+0.5);
+  l->DrawLine(xmin,-0.5,xmax,-0.5);
+
+  tdrDraw(hd,"Pz",kFullCircle,kBlue,kSolid,-1,kNone,0,0.6);
+
+  // Fit nibs
+  TF1 *f1 = new TF1("f1","[0]",xmin,xmax);
+  f1->SetLineColor(kBlue);
+  TMultiGraph *mg = new TMultiGraph();
+  mg->Add(new TGraphErrors(hd));
+  //mg->Draw("SAMEHIST");
+  typedef set<double>::const_iterator IT;
+  double xfit1(0),xfit2(0);
+  double vx[breakset.size()+2];
+  double vy[breakset.size()+1];
+  double vye[breakset.size()+1];
+  int nib(0);
+  vx[0] = xmin;
+  for (IT it = breakset.begin(); it != breakset.end(); ++it) {
+    xfit1 = xfit2;
+    xfit2 = (*it);
+    f1->SetRange(xfit1,xfit2);
+    mg->Fit(f1,"QRN");
+    f1->DrawClone("SAME");
+    if (xfit2!=xfit1) {
+      vx[nib+1] = xfit2;
+      vy[nib] = f1->GetParameter(0);
+      vye[nib] = f1->GetParError(0);
+      ++nib;
+    }
+  }
+  TH1D *hnib = new TH1D("hnib","",nib,&vx[0]);
+  for (int i = 0; i != nib; ++i) {
+    hnib->SetBinContent(i+1, vy[i]);
+    hnib->SetBinError(i+1, vye[i]);
+  }
+  tdrDraw(hnib,"E2",kNone,kCyan+1,kSolid,-1,1001,kCyan+1);
+  hnib->SetFillColorAlpha(kCyan+1,0.5);
+  
+  gPad->RedrawAxis();
+  
+  c1->SaveAs("pdf/drawTimeStability/drawTimeStabilityPairs_GamVsGam.pdf");
+} // drawTimeStabilityPairs()
 
 
 void drawPFcomp(string ref) {
