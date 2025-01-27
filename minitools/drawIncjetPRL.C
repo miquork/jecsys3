8e94450963422ffd99c4bc0d015fcb5d2acf2c61
Mikko Voutilainen <mikko.voutilainen@cern.ch>
2025-01-27 13:48:53 +0200

    Add unfolding to minitools/drawIncJetPRL.C

--- minitools/drawIncjetPRL.C
+++ minitools/drawIncjetPRL.C
@@ -7,6 +7,7 @@
 
 // Normalize histogram for pT and lumi
 void normalizeHist(TH1D *h, double absy, string trigger="");
+void unfoldHist(TH1D *h, TH1D *hu);
 void cleanHist(TH1D *h, double xmin, double xmax, bool filterBins = false);
 
 // Draw trigger efficiency in addition to data/theory ratio
@@ -16,18 +17,120 @@ void drawIncjetPRL() {
 
   setTDRStyle();
   TDirectory *curdir = gDirectory;
+
+  //////////////////////////////////////////////
+  // Step 0: get bin-by-bin unfolding from MC //
+  //////////////////////////////////////////////
+
+  TFile *fm = new TFile("rootfiles/Prompt2024/v114_2024/jmenano_mc_out_Winter24MGV14_v114_2024.root","READ");
+
+  // Data / theory or Data / fit plots
+  TH1D *h_0 = tdrHist("h_0","Gen / Reco",0,2,"Jet p_{T} (GeV)",24,5000);
+  TCanvas *c0 = new TCanvas("c0","c0",5*600,2*600);
+  c0->Divide(5,2,0,0);
+
+  extraText = "Private";
+  lumi_136TeV = "Winter24MGV14";
   
-  //TFile *f = new TFile("rootfiles/Prompt2024/v113_2024/jmenano_data_cmb_2024FGHI_JME_v113.root","READ");
-  //TFile *f = new TFile("rootfiles/Prompt2024/v114_2024/jmenano_data_cmb_2024FGHI_JME_v114.root","READ");
-  TFile *f = new TFile("rootfiles/Prompt2024/v114_2024/jmenano_data_cmb_2024FGHI_JME_v114_2024.root","READ"); // new version
+  TLatex *tex = new TLatex();
+  tex->SetNDC(); tex->SetTextSize(0.045);
+
+  tex->DrawLatex(0.045,0.95,"CMS Private");
+  tex->DrawLatex(0.77,0.95,lumi_136TeV + " (13.6 TeV)");
+
+  const int nybins(10);
+  int marker[nybins] =
+    {kFullSquare, kOpenSquare, kFullCircle, kOpenCircle, kFullSquare,
+     kFullSquare, kOpenSquare, kFullCircle, kOpenCircle, kFullSquare};
+  int color[nybins] =
+    {kBlack, kBlack, kBlack, kBlack, kBlack,
+     kRed, kBlue, kBlue, kBlue, kBlue};
+  double size[nybins] =
+      {0.5, 0.5, 0.5, 0.5, 0.5,
+     0.5, 0.5, 0.5, 0.5, 0.5};
+
+  TLine *l = new TLine();
+  l->SetLineStyle(kDashed);
+  l->SetLineColor(kGray+2);
+
+  fm->cd("HLT_MC");
+  gDirectory->cd("Incjet");
+  gDirectory->cd("Unfolding");
+  TDirectory *dm = gDirectory;
+
+  map<int,TH1D*> munf;
+  for (int ybin = 0; ybin != nybins; ++ybin) {
+
+    int ieta = 5*(ybin+1);
+    double y1 = 0+ybin*0.5;
+    double y2 = 0.5+ybin*0.5;
+    double y = 0.5*(y1+y2);
+
+    // Original spectrum
+    TH1D *hptrec = (TH1D*)dm->Get(Form("hpt%02d_rec",ieta)); assert(hptrec);
+    TH1D *hptgen = (TH1D*)dm->Get(Form("hpt%02d_gen",ieta)); assert(hptgen);
+    TH1D *hptr = (TH1D*)hptgen->Clone(Form("hptr_%d",ybin));
+    hptr->Divide(hptrec);
+    //normalizeHist(hpt, y);
+    //cleanHist(hpt, 28., 6800./cosh(y1));
+
+    TH1D *hptr2 = (TH1D*)hptr->Clone(Form("hptr2_%d",ybin));
+    unfoldHist(hptr2, hptr);
+    hptr2->Divide(hptr);
+    
+    c0->cd(ybin+1);
+    gPad->SetLogx();
+
+    if (ybin<=4) {
+      gPad->SetTopMargin(0.10);
+      h_0->GetYaxis()->SetTitleSize(0.045*1.7);
+      h_0->GetYaxis()->SetLabelSize(0.045*1.7);
+    }
+    if (ybin>=5) {
+      gPad->SetBottomMargin(0.17);
+      h_0->GetYaxis()->SetTitleSize(0.045*1.5);
+      h_0->GetYaxis()->SetLabelSize(0.045*1.5);
+      h_0->GetXaxis()->SetTitleSize(ybin>5 ? 0.045*1.7 : 0.045*1.5);
+      h_0->GetXaxis()->SetLabelSize(ybin>5 ? 0.045*1.7 : 0.045*1.5);
+      h_0->GetXaxis()->SetTitleOffset(ybin>5 ? 1.1 : 1.2);
+    }
+
+    h_0->DrawClone("AXIS");
+    l->DrawLine(28,1.,(ybin==9 ? 200. : 5000.),1.);
+    if (true) {
+      tex->DrawLatex(ybin%5==0 ? 0.25 : 0.10, ybin<5 ? 0.85 : 0.95,
+		     Form("[%1.1f, %1.1f]",y1,y2));
+    }
+      
+    //tdrDraw(hptr,"H][",kNone,color[ybin],kSolid,-1,kNone,0,1.5);
+    //tdrDraw(hrf,"Pz",marker[ybin],color[ybin],kSolid,-1,kNone,0,1.5);
+
+    hptr2->GetXaxis()->SetRangeUser(24,5000);
+    hptr->GetXaxis()->SetRangeUser(24,5000);
+    tdrDraw(hptr2,"H][",kNone,color[ybin],kSolid,-1,kNone,0,1.5);
+    tdrDraw(hptr,"Pz",marker[ybin],color[ybin],kSolid,-1,kNone,0,1.5);
+    
+    //if (y1==0) leg2->AddEntry(hrf,Form("|y| < %1.1f",y2),"PLE");
+    //else leg2->AddEntry(hrf,Form("%1.1f #leq |y| < %1.1f",y1,y2),"PLE");
+    gPad->RedrawAxis();
+
+    munf[ieta] = hptr;
+  } // for ybin
+
+  c0->cd();
+  gPad->RedrawAxis();
+  c0->SaveAs("pdf/drawIncjetPRL/drawIncjetPRL_unfolding.pdf");
+
+  
+  //////////////////////////////////////////
+  // Step 1: draw (unfolded) data spectra //
+  //         (vs theory or fit, or MC)    //
+  //////////////////////////////////////////
+
+  TFile *f = new TFile("rootfiles/Prompt2024/v114_2024/jmenano_data_cmb_2024FGHI_JME_v114_2024.root","READ");
   assert(f && !f->IsZombie());
   curdir->cd();
-
-  //TH1D *h = tdrHist("h","Cross section d#sigma/dp_{T}dy (pb/GeV)",1e-8,1e11,
-  //		    "Jet p_{T} (GeV)",10-1e-3,5000);
-  //TH1D *h = tdrHist("h","Cross section d#sigma/dp_{T}dy (pb/GeV)",
-  //		    1.001e-8,0.999e8,
-  //		    "Jet p_{T} (GeV)",50,5000);
+  
   TH1D *h = tdrHist("h","Cross section d#sigma/dp_{T}dy (pb/GeV)",
   		    1.001e-8,0.999e9,
   		    "Jet p_{T} (GeV)",28,5000);
@@ -36,42 +139,41 @@ void drawIncjetPRL() {
 
   // Spectra all on the same plot
   TCanvas *c1 = tdrCanvas("c1",h,8,11,kSquare);
-  //h->GetXaxis()->SetMoreLogLabels(kFALSE);
   h->GetYaxis()->SetTitleOffset(1.19);
   c1->SetLogx();
   c1->SetLogy();
 
+  
+  //////////////////////////////////////////
+  // Step 2: draw (unfolded) data spectra //
+  //         over theory (or fit, or MC)  //
+  //////////////////////////////////////////
+  
   // Data / theory or Data / fit plots
-  //TH1D *h2 = tdrHist("h2","Data / fit",0,2,"Jet p_{T} (GeV)",28,5000);
   TH1D *h2 = tdrHist("h2","Data / fit",0,2,"Jet p_{T} (GeV)",24,5000);
   TCanvas *c2 = new TCanvas("c2","c2",5*600,2*600);
   c2->Divide(5,2,0,0);
 
-  TLatex *tex = new TLatex();
-  tex->SetNDC(); tex->SetTextSize(0.045);
+  //TLatex *tex = new TLatex();
+  //tex->SetNDC(); tex->SetTextSize(0.045);
 
   tex->DrawLatex(0.045,0.95,"CMS Private");
   tex->DrawLatex(0.77,0.95,lumi_136TeV + " (13.6 TeV)");
 
-  
-  const int nybins(10);
-  int marker[nybins] =
-  //{kFullSquare, kOpenSquare, kFullCircle, kOpenCircle, kFullDiamond,
-  // kFullSquare, kOpenSquare, kFullCircle, kOpenCircle, kFullDiamond};
-    {kFullSquare, kOpenSquare, kFullCircle, kOpenCircle, kFullSquare,
-     kFullSquare, kOpenSquare, kFullCircle, kOpenCircle, kFullSquare};
-  int color[nybins] =
-    {kBlack, kBlack, kBlack, kBlack, kBlack,
-     kRed, kBlue, kBlue, kBlue, kBlue};
-  double size[nybins] =
-    //{0.5, 0.5, 0.5, 0.5, 0.8,
-  //0.5, 0.5, 0.5, 0.5, 0.8};
-      {0.5, 0.5, 0.5, 0.5, 0.5,
-     0.5, 0.5, 0.5, 0.5, 0.5};
+  /* const int nybins(10); */
+  /* int marker[nybins] = */
+  /*   {kFullSquare, kOpenSquare, kFullCircle, kOpenCircle, kFullSquare, */
+  /*    kFullSquare, kOpenSquare, kFullCircle, kOpenCircle, kFullSquare}; */
+  /* int color[nybins] = */
+  /*   {kBlack, kBlack, kBlack, kBlack, kBlack, */
+  /*    kRed, kBlue, kBlue, kBlue, kBlue}; */
+  /* double size[nybins] = */
+  /*     {0.5, 0.5, 0.5, 0.5, 0.5, */
+  /*    0.5, 0.5, 0.5, 0.5, 0.5}; */
 
-  TLine *l = new TLine();
-  l->SetLineStyle(kDashed);
-  l->SetLineColor(kGray+2);
+  //TLine *l = new TLine();
+  //l->SetLineStyle(kDashed);
+  //l->SetLineColor(kGray+2);
   
   c1->cd();
   TLegend *leg = tdrLeg(0.85-0.25,0.90-0.035*nybins,0.85,0.90);
@@ -91,11 +193,11 @@ void drawIncjetPRL() {
     TH1D *hpt = (TH1D*)f->Get(Form("Incjet/hpt%02d",ieta)); assert(hpt);
     hpt = (TH1D*)hpt->Clone(Form("hpt_%d",ybin));
     normalizeHist(hpt, y);
-    //cleanHist(hpt, 50., 13000.);
-    //cleanHist(hpt, 28., 13600.);
     cleanHist(hpt, 28., 6800./cosh(y1));
-    //if (ieta==30) cleanHist(hpt, 50, 13600.); 
 
+    // Bin-by-bin unfolding
+    unfoldHist(hpt, munf[ieta]);
+    
     TH1D *hptf = (TH1D*)hpt->Clone(Form("hptf_%d",ybin));
     cleanHist(hptf, 28, 13600., true);
     
@@ -103,13 +205,8 @@ void drawIncjetPRL() {
     TF1 *f1 = new TF1(Form("f1_%d",ybin),
 		      Form("[0]*pow(x-[3],[1])*pow(1-x*%1.4g,[2])",
 			   2.*cosh(y1)/13600.),28,6800./cosh(y1+0.125));
-    //2.*cosh(y1)/13600.),28,6800./cosh(y1));
-    //2.*cosh(y2)/13600.),28,6800./cosh(y2));
-    //2.*cosh(y)/13600.),28,6800./cosh(y));
     f1->SetParameters(1e11,-5,10,0.);
-    //if (ybin==9) f1->SetRange(49, 6800./cosh(y1));
     if (ybin>4) {
-    //f1->SetRange(84, 6800./cosh(y1));
       f1->FixParameter(3,0.);
     }
     hptf->Fit(f1,"QRN");
@@ -199,14 +296,15 @@ void drawIncjetPRL() {
   c1->SaveAs("pdf/drawIncjetPRL/drawIncjetPRL_spectra.pdf");
 
   c2->cd(10);
-  //TLegend *leg2 = (TLegend*)leg->Clone();
-  //leg2->SetTextSize(0.045*1.5);
-  //leg2->SetX1NDC(0.85-(0.25+0.05)*1.5);
-  //leg2->SetY1NDC(0.90-nybins*0.035*1.5);
   leg2->Draw();
   c2->SaveAs("pdf/drawIncjetPRL/drawIncjetPRL_ratio.pdf");
 
-
+  
+  ////////////////////////////////////////////////////////////
+  // Step 3: Check trigger spectra vs combined spectrum     //
+  //         This is basic sanity check of trigger turn-ons //
+  ////////////////////////////////////////////////////////////
+  
   if (drawTrigEff) {
 
     c2->cd(10);
@@ -454,6 +552,38 @@ void normalizeHist(TH1D *h, double absy, string trg) {
   
 } // normalizeHist
 
+void unfoldHist(TH1D *h, TH1D *hu) {
+
+  for (int i = 1; i != h->GetNbinsX()+1; ++i) {
+    double x = h->GetBinCenter(i);
+    double y = h->GetBinContent(i);
+    double ey = h->GetBinError(i);
+    double r(1), er(0);
+    if (x>84) {
+      r = hu->GetBinContent(i);
+      er = hu->GetBinError(i);
+    }
+    else {
+      int j = hu->GetXaxis()->FindBin(84.+1);
+      double r1 = hu->GetBinContent(j);
+      double r2 = hu->GetBinContent(j+1);
+      //r = max(0.05,r1 - (j-i)*(r2-r1));
+      r = r1*pow(r1/r2,j-i);
+      er = sqrt(pow(er,2)+pow(r-r1,2));
+    }
+    //if (y>0 && r>0) {
+    //if (y>0 && r>0.1) {
+    if (y>0 && r>0.25) {
+    //if (y>0 && r>1./3.) {
+      h->SetBinContent(i, y * r);
+      h->SetBinError(i, y * r * sqrt(pow(ey/y,2)+pow(er/r,2)));
+    }
+    else {
+      h->SetBinContent(i, 0.);
+      h->SetBinError(i, 0.);
+    }
+  } // for i in h
+} // unfoldHist
 
 void cleanHist(TH1D *h, double xmin, double xmax, bool filterBins) {
 
@@ -478,7 +608,8 @@ void cleanHist(TH1D *h, double xmin, double xmax, bool filterBins) {
 	   //(ybin<5 && (x>49 && x<57)) ||
 	   //(ybin>5 && (x>49 && x<57)) ||
 	   //(ybin==9 && x<49) ) {
-	   (ybin==9 && x<56) ) {
+	   //(ybin==9 && x<56) ) {
+	   (ybin==9 && x<37) ) {
 	h->SetBinContent(i, 0.);
 	h->SetBinError(i, 0.);
       }
