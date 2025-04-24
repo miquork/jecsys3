c9e8044de62b62019bcdc9696bd1dbbe55935c70
Mikko Voutilainen <voutila@Mikkos-MacBook-Air.local>
2025-04-24 16:48:47 +0300

    Update minitools/drawIncjetPRL.C with better fit and newer data.

--- minitools/drawIncjetPRL.C
+++ minitools/drawIncjetPRL.C
@@ -13,6 +13,37 @@ void cleanHist(TH1D *h, double xmin, double xmax, bool filterBins = false);
 // Draw trigger efficiency in addition to data/theory ratio
 const bool drawTrigEff = true;
 
+// Fit function with some granularity vs eta
+Double_t fitFunc(Double_t *xx, Double_t *p) {
+
+  // Reference fit:
+  // Form("[0]*pow(x-[3],[1])*pow(1-x*%1.4g,[2])"
+  // 2.*cosh(y1)/13600.),28,6800./cosh(y1+0.125));
+  double x = xx[0]; // pt
+  double y1 = p[4];
+  double y2 = p[5];
+  assert(y2>y1);
+
+  const int ny = 20;
+  double dy = (y2-y1)/ny;
+  double sumf(0);
+  for (int i = 0; i != ny; ++i) {
+
+    //double y = y1 + dy*i+0.5*dy;
+    double y = y1 + dy*i;
+    double erg = x*cosh(y);
+    //double f = (erg<6800 ? p[0]*pow(x-p[3],p[1])*pow(1-erg/6800.,p[2]) : 0.);
+    //double f = p[0]*pow(x-p[3],p[1])*pow(1-x*2.*cosh(y1)/13600.,p[2]);
+    //double f = (erg<6800. ? p[0]*pow(x-p[3],p[1])*pow(1-x*2.*cosh(y)/13600.,p[2]) : 0);
+    double f = (erg<6800. ? p[0]*pow(x-p[3],p[1])*pow(1-erg/6800.,p[2]) : 0);
+    sumf += f * dy;
+  }
+
+  return (sumf / (y2-y1));
+
+  //return (p[0]*pow(x-p[3],p[1])*pow(1-x*2.*cosh(y1)/13600.,p[2]));
+} // fitFunc
+       
 void drawIncjetPRL() {
 
   setTDRStyle();
@@ -58,6 +89,7 @@ void drawIncjetPRL() {
   gDirectory->cd("Unfolding");
   TDirectory *dm = gDirectory;
 
+  map<int,TH1D*> mmc;
   map<int,TH1D*> munf;
   for (int ybin = 0; ybin != nybins; ++ybin) {
 
@@ -74,6 +106,10 @@ void drawIncjetPRL() {
     //normalizeHist(hpt, y);
     //cleanHist(hpt, 28., 6800./cosh(y1));
 
+    TH1D *hmc = (TH1D*)hptgen->Clone(Form("hmc_%d",ybin));
+    hmc->Scale(1,"width");
+    mmc[ieta] = hmc;
+    
     TH1D *hptr2 = (TH1D*)hptr->Clone(Form("hptr2_%d",ybin));
     unfoldHist(hptr2, hptr);
     hptr2->Divide(hptr);
@@ -127,13 +163,17 @@ void drawIncjetPRL() {
   //         (vs theory or fit, or MC)    //
   //////////////////////////////////////////
 
-  TFile *f = new TFile("rootfiles/Prompt2024/v114_2024/jmenano_data_cmb_2024FGHI_JME_v114_2024.root","READ");
+  // Rerun combination with minitools/DijetHistosCombine.C if needed:
+  //TFile *f = new TFile("rootfiles/Prompt2024/v114_2024/jmenano_data_cmb_2024FGHI_JME_v114_2024.root","READ");
+  TFile *f = new TFile("rootfiles/Prompt2024/v116_Jet/jmenano_data_cmb_2024FGHI_JME_v116.root","READ"); // V8M+PtRaw
   assert(f && !f->IsZombie());
   curdir->cd();
   
   TH1D *h = tdrHist("h","Cross section d#sigma/dp_{T}dy (pb/GeV)",
-  		    1.001e-8,0.999e9,
-  		    "Jet p_{T} (GeV)",28,5000);
+  		    //1.001e-8,0.999e9,
+		    1.001e-8,0.999e10,
+  		    //"Jet p_{T} (GeV)",28,5000);
+		    "Jet p_{T} (GeV)",25,5000);
   extraText = "Private";
   lumi_136TeV = "Late 2024, 82.4 fb^{-1}";
 
@@ -182,6 +222,7 @@ void drawIncjetPRL() {
   TLegend *leg2 = tdrLeg(0.85-0.25*1.5,0.90-0.035*nybins*1.5,0.85,0.90);
   leg2->SetTextSize(0.040*1.5);
 
+  vector<TF1*> vf1(nybins);
   for (int ybin = 0; ybin != nybins; ++ybin) {
 
     int ieta = 5*(ybin+1);
@@ -200,6 +241,9 @@ void drawIncjetPRL() {
     
     TH1D *hptf = (TH1D*)hpt->Clone(Form("hptf_%d",ybin));
     cleanHist(hptf, 28, 13600., true);
+
+    TH1D *hmc = mmc[ieta];
+    cleanHist(hmc, 28, 13600., false);
     
     // Fits for ratio
     TF1 *f1 = new TF1(Form("f1_%d",ybin),
@@ -211,9 +255,34 @@ void drawIncjetPRL() {
     }
     hptf->Fit(f1,"QRN");
     hptf->Fit(f1,"QRNM");
-    
+
+    // More advanced fits for ratio
+    TF1 *f2 = new TF1(Form("f2_%d",ybin), fitFunc, //28, 6800, 6);
+		      28, 6800./cosh(y1+0.125), 6);
+    f2->SetParameters(f1->GetParameter(0),f1->GetParameter(1),
+		      f1->GetParameter(2), f1->GetParameter(3), y1,y2);
+    f2->FixParameter(4, y1);
+    f2->FixParameter(5, y2);
+    if (ybin>4) {
+      f2->FixParameter(3,0.);
+    }
+    //else
+    //f2->SetParLimits(3, -4,4);
+    //if (ybin==9) {
+    //f2->FixParameter(1,-3.);
+    //}
+    hptf->Fit(f2,"QRN");
+    //f2->FixParameter(3,0.);
+    //hptf->Fit(f2,"QRN");
+    //f2->FixParameter(1,-5.);
+    hptf->Fit(f2,"QRN");
+    // Integral: slow, but more accurate for later
+    hptf->Fit(f2,"QRNI");
+    vf1[ybin] = f2;
 
     c1->cd();
+    tdrDraw(hmc,"H][",kNone,color[ybin],kSolid,-1,kNone,0,size[ybin]);
+    //hmc->SetLineWidth(2);
     tdrDraw(hpt,"H][",kNone,color[ybin],kSolid,-1,kNone,0,size[ybin]);
     tdrDraw(hptf,"Pz",marker[ybin],color[ybin],kSolid,-1,kNone,0,size[ybin]);
 
@@ -221,7 +290,12 @@ void drawIncjetPRL() {
     else leg->AddEntry(hptf,Form("%1.1f #leq |y| < %1.1f",y1,y2),"PLE");
 
     f1->SetLineColor(hpt->GetLineColor());
+    f1->SetLineStyle(kDotted);
     f1->Draw("SAME");
+
+    f2->SetLineColor(hpt->GetLineColor());
+    //f2->SetLineWidth(2);
+    f2->Draw("SAME");
     
 
     c2->cd(ybin+1);
@@ -229,6 +303,7 @@ void drawIncjetPRL() {
 
     TH1D *hr = (TH1D*)hpt->Clone(Form("hr_%d",ybin));
     TH1D *hrf = (TH1D*)hptf->Clone(Form("hrf_%d",ybin));
+    TH1D *hrmc = (TH1D*)hmc->Clone(Form("hrmc_%d",ybin));
     for (int i = 1; i != hr->GetNbinsX()+1; ++i) {
       double x1 = min(hr->GetBinLowEdge(i),6800./cosh(y1));
       double x2 = min(hr->GetBinLowEdge(i+1),6800./cosh(y1));
@@ -237,14 +312,19 @@ void drawIncjetPRL() {
 	hr->SetBinError(i, 0.);
 	hrf->SetBinContent(i, 0.);
 	hrf->SetBinError(i, 0.);
+	hrmc->SetBinContent(i, 0.);
+	hrmc->SetBinError(i, 0.);
 	continue;
       }
-      double xsec = f1->Integral(x1, x2) / (x2 - x1);
+      //double xsec = f1->Integral(x1, x2) / (x2 - x1);
+      double xsec = f2->Integral(x1, x2) / (x2 - x1);
       if (xsec>0) {
 	hr->SetBinContent(i, hpt->GetBinContent(i) / xsec);
 	hr->SetBinError(i, hpt->GetBinError(i) / xsec);
 	hrf->SetBinContent(i, hptf->GetBinContent(i) / xsec);
 	hrf->SetBinError(i, hptf->GetBinError(i) / xsec);
+	hrmc->SetBinContent(i, hmc->GetBinContent(i) / xsec);
+	hrmc->SetBinError(i, hmc->GetBinError(i) / xsec);
       }
     } // for i
 
@@ -271,19 +351,25 @@ void drawIncjetPRL() {
 		     Form("[%1.1f, %1.1f]",y1,y2));
       tex->DrawLatex(ybin%5==0 ? 0.25 : 0.10, ybin<5 ? 0.80 : 0.90,
 		     Form("N_{0} = %1.3g #pm %1.3g",
-			  f1->GetParameter(0), f1->GetParError(0)));
+			  //f1->GetParameter(0), f1->GetParError(0)));
+			  f2->GetParameter(0), f2->GetParError(0)));
       tex->DrawLatex(ybin%5==0 ? 0.25 : 0.10, ybin<5 ? 0.75 : 0.85,
 		     Form("#alpha = %1.3f #pm %1.3f",
-			  f1->GetParameter(1), f1->GetParError(1)));
+			  //f1->GetParameter(1), f1->GetParError(1)));
+			  f2->GetParameter(1), f2->GetParError(1)));
       tex->DrawLatex(ybin%5==0 ? 0.25 : 0.10, ybin<5 ? 0.70 : 0.80,
 		     Form("#beta = %1.2f #pm %1.2f",
-			  f1->GetParameter(2), f1->GetParError(2)));
-      if (f1->GetParameter(3)!=0 || f1->GetParError(3)!=0)
+			  //f1->GetParameter(2), f1->GetParError(2)));
+			  f2->GetParameter(2), f2->GetParError(2)));
+      //if (f1->GetParameter(3)!=0 || f1->GetParError(3)!=0)
+      if (f2->GetParameter(3)!=0 || f2->GetParError(3)!=0)
 	tex->DrawLatex(ybin%5==0 ? 0.25 : 0.10, ybin<5 ? 0.65 : 0.75,
 		       Form("#Deltap_{T} = %1.2f #pm %1.2f GeV",
-			    f1->GetParameter(3), f1->GetParError(3)));
+			    //f1->GetParameter(3), f1->GetParError(3)));
+			    f2->GetParameter(3), f2->GetParError(3)));
     }
-      
+
+    tdrDraw(hrmc,"H][",kNone,color[ybin],kSolid,-1,kNone,0,1.5);
     tdrDraw(hr,"H][",kNone,color[ybin],kSolid,-1,kNone,0,1.5);
     tdrDraw(hrf,"Pz",marker[ybin],color[ybin],kSolid,-1,kNone,0,1.5);
     if (y1==0) leg2->AddEntry(hrf,Form("|y| < %1.1f",y2),"PLE");
@@ -299,6 +385,23 @@ void drawIncjetPRL() {
   leg2->Draw();
   c2->SaveAs("pdf/drawIncjetPRL/drawIncjetPRL_ratio.pdf");
 
+  // Bonus: calculate probability of another pileup interaction being harder
+  //        with jet at |eta|<2.5, which causes inefficiency with PUPPI
+  TH1D *hmiss = (TH1D*)munf[5]->Clone("hmiss");
+  for (int i = 1; i != hmiss->GetNbinsX()+1; ++i) {
+    double ptmin = hmiss->GetBinLowEdge(i);
+    double sumf(0);
+    for (int ybin = 0; ybin != 5; ++ybin) {
+      TF1 *f1 = vf1[ybin]; assert(tf1);
+      sumf += f1->Integral(ptmin,6800.);
+    } // for ybin
+    hmiss->SetBinContent(i, 60.*sumf*1e-12 / 80e-3);
+  } // for i
+
+  c2->cd(1);
+  tdrDraw(hmiss,"H][",kNone,kRed,kSolid,-1,kNone,0,1.5);
+  c2->SaveAs("pdf/drawIncjetPRL/drawIncjetPRL_ratio_hmiss.pdf");
+
   
   ////////////////////////////////////////////////////////////
   // Step 3: Check trigger spectra vs combined spectrum     //
@@ -310,7 +413,8 @@ void drawIncjetPRL() {
     c2->cd(10);
   
     cout << "Do drawTrigEff" << endl << flush;
-    TFile *ft = new TFile("rootfiles/Prompt2024/v114_2024//jmenano_data_out_2024FGHI_JME_v114_2024.root","READ");
+    //TFile *ft = new TFile("rootfiles/Prompt2024/v114_2024//jmenano_data_out_2024FGHI_JME_v114_2024.root","READ");
+    TFile *ft = new TFile("rootfiles/Prompt2024/v116_Jet/jmenano_data_out_2024FGHI_JME_v116.root","READ"); // V8M+PtRaw
     assert(ft && !ft->IsZombie());
 
     string vt[] =
@@ -416,7 +520,7 @@ void normalizeHist(TH1D *h, double absy, string trg) {
 
     // HLT_ZeroBias_v* fudge factor for luminosity
     //double kzb = 0.5;
-    double kzb = 0.5*0.9;
+    double kzb = 0.5*0.82;//*0.9;
     double kfwd80 = 0.5*1.3;//1.2;//1.1;//*0.9;
     double kfwd140 = 0.5*1.3;//1.1;
     double kfwd200 = 0.5*1.3;
