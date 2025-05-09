f87f245719924d4e7fdfb73c0376e4f471792eae
Voutilainen <manvouti@dm4-915-32460.pc.helsinki.fi>
2025-05-09 17:51:17 +0300

    Store test/mk_drawCMSresponse.C for V8M.

--- test/mk_drawCMSresponse.C
+++ test/mk_drawCMSresponse.C
@@ -4,100 +4,28 @@
   gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/SimpleJetCorrector.cc+");
   gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/FactorizedJetCorrector.cc+");
 
-  gROOT->ProcessLine(".L test/drawCMSresponse.C+");
+  gROOT->ProcessLine(".L test/drawCMSresponse.C+g");
 
   gROOT->ProcessLine(".! touch pdf/test");
+
+  for (int i = 0; i != 2; ++i) {
+  Bool_t l2res = (i!=0);
+  drawCMSresponse("Prompt24_Run2024B_nib1_V8M","Prompt24B_nib1_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024C_nib1_V8M","Prompt24C_nib1_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024D_nib1_V8M","Prompt24D_nib1_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024Ev1_nib1_V8M","Prompt24Ev1_nib1_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024Ev2_nib1_V8M","Prompt24Ev2_nib1_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024F_nib1_V8M","Prompt24F_nib1_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024F_nib2_V8M","Prompt24F_nib2_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024F_nib3_V8M","Prompt24F_nib3_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024G_nib1_V8M","Prompt24G_nib1_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024G_nib2_V8M","Prompt24G_nib2_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024H_nib1_V8M","Prompt24H_nib1_V8M",l2res);
+  drawCMSresponse("Prompt24_Run2024I_nib1_V8M","Prompt24I_nib1_V8M",l2res);
+  }
   
-  //drawCMSresponse();
-  /*
-  drawCMSresponse("Summer22-22Sep2023_Run2022CD_V3","22CD-22Sep2023_V3",false);
-  drawCMSresponse("Summer22-22Sep2023_Run2022CD_V3","22CD-22Sep2023_V3",true);
-  drawCMSresponse("Summer22EE-22Sep2023_Run2022E_V3","22E-22Sep2023_V3",false);
-  drawCMSresponse("Summer22EE-22Sep2023_Run2022E_V3","22E-22Sep2023_V3",true);
-  drawCMSresponse("Summer22EEPrompt22_Run2022F_V3","22F-Prompt_V3",false);
-  drawCMSresponse("Summer22EEPrompt22_Run2022F_V3","22F-Prompt_V3",true);
-  drawCMSresponse("Summer22EEPrompt22_Run2022G_V3","22G-Prompt_V3",false);
-  drawCMSresponse("Summer22EEPrompt22_Run2022G_V3","22G-Prompt_V3",true);
-  drawCMSresponse("Summer22Prompt23_Run2023Cv123_V3","23C123-Prompt_V3",false);
-  drawCMSresponse("Summer22Prompt23_Run2023Cv123_V3","23C123-Prompt_V3",true);
-  drawCMSresponse("Summer22Prompt23_Run2023Cv4_V3","23C4-Prompt_V3",false);
-  drawCMSresponse("Summer22Prompt23_Run2023Cv4_V3","23C4-Prompt_V3",true);
-  drawCMSresponse("Summer22Prompt23_Run2023D_V3","23D-Prompt_V3",false);
-  drawCMSresponse("Summer22Prompt23_Run2023D_V3","23D-Prompt_V3",true);
-  */
-  /*
-  drawCMSresponse("Run22CD-22Sep2023_DATA","22CD-22Sep2023");
-  drawCMSresponse("Run22E-22Sep2023_DATA","22E-22Sep2023");
-  drawCMSresponse("Run22F-Prompt_DATA","22F-Prompt");
-  drawCMSresponse("Run22G-Prompt_DATA","22G-Prompt");
-  drawCMSresponse("Run23C123-Prompt_DATA","23C123-Prompt");
-  drawCMSresponse("Run23C4-Prompt_DATA","23C4-Prompt");
-  drawCMSresponse("Run23D-Prompt_DATA","23D-Prompt");
-  drawCMSresponse("Run23C4D-Prompt_DATA","23C4D-Prompt");
-  */
-
-  /*
-  drawCMSresponse("Summer23Prompt23_Run2023Cv123_V1","23C123-Summer23_V2",true);
-  drawCMSresponse("Summer23Prompt23_Run2023Cv4_V1","23C4-Summer23_V2",true);
-  drawCMSresponse("Summer23Prompt23_Run2023D_V1","23D-Summer23_V2",true);
-
-  drawCMSresponse("Summer23Prompt23_Run2023Cv123_V2","23C123-Summer23_V2",false);
-  drawCMSresponse("Summer23Prompt23_Run2023Cv4_V2","23C4-Summer23_V2",false);
-  drawCMSresponse("Summer23Prompt23_Run2023D_V2","23D-Summer23_V2",false);
-  */
-
-  //drawCMSresponse("Prompt24_Run2024BC_V1M","Prompt24BC_V1M",true);
-  //drawCMSresponse("Prompt24_Run2024BC_V1M","Prompt24BC_V1M",false);
-  //drawCMSresponse("Prompt24_Run2024BC_V2M","Prompt24BC_V2M",true);
-  //drawCMSresponse("Prompt24_Run2024BC_V2M","Prompt24BC_V2M",false);
-
-  //drawCMSresponse("Summer23Prompt23_Run2023D_V2xHFscale","23D_V2xHF",false);
-
-  /*
-  drawCMSresponse("Prompt24_Run2024BCD_V3M","Prompt24BCD_V3M",true);
-  drawCMSresponse("Prompt24_Run2024BCD_V3M","Prompt24BCD_V3M",false);
-  drawCMSresponse("Prompt24_Run2024CR_V3M","Prompt24CR_V3M",true);
-  drawCMSresponse("Prompt24_Run2024CR_V3M","Prompt24CR_V3M",false);
-  */
-  /*
-  drawCMSresponse("Prompt24_Run2024BCD_V4M","Prompt24BCD_V4M",true);
-  drawCMSresponse("Prompt24_Run2024BCD_V4M","Prompt24BCD_V4M",false);
-  drawCMSresponse("Prompt24_Run2024E_V4M","Prompt24E_V4M",true);
-  drawCMSresponse("Prompt24_Run2024E_V4M","Prompt24E_V4M",false);
-  drawCMSresponse("Prompt24_Run2024CR_V4M","Prompt24CR_V4M",true);
-  drawCMSresponse("Prompt24_Run2024CR_V4M","Prompt24CR_V4M",false);
-  drawCMSresponse("Prompt24_Run2024CS_V4M","Prompt24CS_V4M",true);
-  drawCMSresponse("Prompt24_Run2024CS_V4M","Prompt24CS_V4M",false);
-  */
-  /*
-  drawCMSresponse("Prompt24_Run2024BCD_V5M","Prompt24BCD_V5M",true);
-  drawCMSresponse("Prompt24_Run2024BCD_V5M","Prompt24BCD_V5M",false);
-  drawCMSresponse("Prompt24_Run2024E_V5M","Prompt24E_V5M",true);
-  drawCMSresponse("Prompt24_Run2024E_V5M","Prompt24E_V5M",false);
-  drawCMSresponse("Prompt24_Run2024F_V5M","Prompt24F_V5M",true);
-  drawCMSresponse("Prompt24_Run2024F_V5M","Prompt24F_V5M",false);
-  */
-  /*
-  drawCMSresponse("Prompt24_Run2024BCD_V6M","Prompt24BCD_V6M",true);
-  drawCMSresponse("Prompt24_Run2024BCD_V6M","Prompt24BCD_V6M",false);
-  drawCMSresponse("Prompt24_Run2024E_V6M","Prompt24E_V6M",true);
-  drawCMSresponse("Prompt24_Run2024E_V6M","Prompt24E_V6M",false);
-  drawCMSresponse("Prompt24_Run2024F_V6M","Prompt24F_V6M",true);
-  drawCMSresponse("Prompt24_Run2024F_V6M","Prompt24F_V6M",false);
-  drawCMSresponse("Prompt24_Run2024G_V6M","Prompt24G_V6M",true);
-  drawCMSresponse("Prompt24_Run2024G_V6M","Prompt24G_V6M",false);
-  */
+  //drawCMSresponse("Winter24Run3_V1_MC","Winter24Run3_V1_MC", false);
+  //drawCMSresponse("Winter24Run3_V1_MC","Winter24Run3_V1_MC", true);
   
-  drawCMSresponse("Prompt24_Run2024BCD_V7M","Prompt24BCD_V7M",true);
-  drawCMSresponse("Prompt24_Run2024BCD_V7M","Prompt24BCD_V7M",false);
-  drawCMSresponse("Prompt24_Run2024E_V7M","Prompt24E_V7M",true);
-  drawCMSresponse("Prompt24_Run2024E_V7M","Prompt24E_V7M",false);
-  drawCMSresponse("Prompt24_Run2024F_V7M","Prompt24F_V7M",true);
-  drawCMSresponse("Prompt24_Run2024F_V7M","Prompt24F_V7M",false);
-  drawCMSresponse("Prompt24_Run2024G_V7M","Prompt24G_V7M",true);
-  drawCMSresponse("Prompt24_Run2024G_V7M","Prompt24G_V7M",false);
-  drawCMSresponse("Prompt24_Run2024H_V7M","Prompt24H_V7M",true);
-  drawCMSresponse("Prompt24_Run2024H_V7M","Prompt24H_V7M",false);
-  drawCMSresponse("Prompt24_Run2024I_V7M","Prompt24I_V7M",true);
-  drawCMSresponse("Prompt24_Run2024I_V7M","Prompt24I_V7M",false);
+  
 }
