46b860de84097a19bf5e11d25a2f4128701f1d02
Mikko Voutilainen <voutila@Mikkos-MacBook-Air.local>
2025-06-25 13:37:25 +0300

    Replace buggy JER SF files for Zmm and Jet with PU-reweighing-only inputs in ConfigFiles.C.

--- ConfigFiles.C
+++ ConfigFiles.C
@@ -9,24 +9,24 @@ std::map<std::string, map<std::string, map<std::string, std::string> > > mf;
 // 2024 partial re-reco + Summer24
 string t = "24V9M";
 
-// Options for Zmm: v100. v100noPU_smear. v100pu_smear.root
+// Options for Zmm: v100. v100pu. v100noPU_smear. v100pu_smear.root
 // Use find-and-replace to switch
 // v100*smear.root are corrupt, bad JRV9M file
-mf[t]["Zmm"]["2024C_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024C_nib1_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024D_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024D_nib1_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024E_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024E_nib1_Zmm_V9M_v100.root";
-//mf[t]["Zmm"]["2024Ev1_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024Ev1_nib1_Zmm_V9M_v100.root";
-//mf[t]["Zmm"]["2024Ev2_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024Ev2_nib1_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024F_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024F_nib1_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024F_nib2"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024F_nib2_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024F_nib3"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024F_nib3_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024G_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024G_nib1_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024G_nib2"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024G_nib2_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024H_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024H_nib1_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024I_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024I_nib1_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024CDE_nib"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDE_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024FGHI_nib"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024FGHI_Zmm_V9M_v100.root";
-mf[t]["Zmm"]["2024_nib"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024_Zmm_V9M_v100.root";
+mf[t]["Zmm"]["2024C_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024C_nib1_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024D_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024D_nib1_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024E_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024E_nib1_Zmm_V9M_v100pu.root";
+//mf[t]["Zmm"]["2024Ev1_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024Ev1_nib1_Zmm_V9M_v100pu.root";
+//mf[t]["Zmm"]["2024Ev2_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDEReprocessing_v1_2024Ev2_nib1_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024F_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024F_nib1_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024F_nib2"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024F_nib2_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024F_nib3"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024F_nib3_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024G_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024G_nib1_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024G_nib2"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024G_nib2_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024H_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024H_nib1_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024I_nib1"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024I_nib1_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024CDE_nib"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024CDE_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024FGHI_nib"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024FGHI_Zmm_V9M_v100pu.root";
+mf[t]["Zmm"]["2024_nib"] = "rootfiles/Prompt2024/Zmm_v100/jme_Zj_2024_Zmm_V9M_v100pu.root";
 mf[t]["Zmm"]["MC"] = ""; // 'mc' directory in input files
 
 mf[t]["Gam"]["2024C_nib1"] = "rootfiles/Prompt2024/Gam_w56/GamHistosFill_data_2024C-rereco_w56.root";
@@ -62,7 +62,8 @@ mf[t]["Jet"]["2024CDE_nib"]   = "rootfiles/Prompt2024/Jet_v134/jmenano_data_cmb_
 mf[t]["Jet"]["2024FGHI_nib"]   = "rootfiles/Prompt2024/Jet_v134/jmenano_data_cmb_2024FGHI_JME_v134.root";
 mf[t]["Jet"]["2024_nib"]   = "rootfiles/Prompt2024/Jet_v134/jmenano_data_cmb_2024CDEFGHI_JME_v134.root";
 //mf[t]["Jet"]["MC"] = "rootfiles/Prompt2024/Jet_v132/jmenano_mc_out_Summer24MG_v132.root"; // no JER SF? no PU RW?
-mf[t]["Jet"]["MC"] = "rootfiles/Prompt2024/Jet_v134/jmenano_mc_out_Summer24MG_v134.root"; // no JER SF, no PU RW
+//mf[t]["Jet"]["MC"] = "rootfiles/Prompt2024/Jet_v134/jmenano_mc_out_Summer24MG_v134.root"; // no JER SF, no PU RW
+mf[t]["Jet"]["MC"] = "rootfiles/Prompt2024/Jet_v134/jmenano_mc_out_Summer24MG_PU69_v134.root"; // no JER SF, with PU RW
 //mf[t]["Jet"]["MC"] = "rootfiles/Prompt2024/Jet_v134/jmenano_mc_out_Summer24MG_JERSF_v134.root"; // with JER SF, no PU RW
 
 
